e1fa4b1321dd5af4a161c1cbb078e1dd46b76107
Esteban Puma <puma.esteban18@gmail.com>
2022-06-01 01:15:53 -0300

    Subo repositorio, primera versión sin habilidades escritas

---
+++ home.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["", "Diseñador Web", "Diseñador Responsive", "Diseñador Grafico", "Comunity Manager"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
