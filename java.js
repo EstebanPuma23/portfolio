0e62ca4d6be07dab55217e937140fbc6a6d29fea
Esteban Puma <puma.esteban18@gmail.com>
2022-06-09 18:12:01 -0300

    modifico java.js

--- java.js
+++ java.js
@@ -13,7 +13,7 @@ const $form = document.querySelector('#form');
         function handleSubmit(e){
             e.preventDefault()
             const form = new FormData(this)
-            $buttomMailTo.setAttribute('href', `mailto:puma.esteban18@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+            $buttomMailTo.setAttribute('href', `mailto:puma.esteban18@gmail.com?subject=${form.get('name')} ${form.get('email')}&body=${form.get('message')}`)
             $buttomMailTo.click()
         }   
 
