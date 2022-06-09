be0297d39f028167d5295296eae3ab93559284f1
Esteban Puma <puma.esteban18@gmail.com>
2022-06-09 17:41:57 -0300

    Subo formulario de contacto responsivo

--- java.js
+++ java.js
@@ -4,4 +4,17 @@ btn.addEventListener("click", function() {
     about.style.display = 'block'
 
 
-})
+});
+
+
+const $form = document.querySelector('#form');
+        const $buttomMailTo = document.querySelector('#trucazo');
+
+        function handleSubmit(e){
+            e.preventDefault()
+            const form = new FormData(this)
+            $buttomMailTo.setAttribute('href', `mailto:puma.esteban18@gmail.com?subject=${form.get('name')}${form.get('email')}&body=${form.get('message')}`)
+            $buttomMailTo.click()
+        }   
+
+        $form.addEventListener('submit',handleSubmit )
